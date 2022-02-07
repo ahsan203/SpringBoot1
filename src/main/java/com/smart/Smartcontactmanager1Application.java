bbb65ddbd28d1be864cfe64316833f2f96b3d749
ahsan203 <45669964+ahsan203@users.noreply.github.com>
2022-02-06 20:23:12 -0600

    comment line added by developer-2

--- src/main/java/com/smart/Smartcontactmanager1Application.java
+++ src/main/java/com/smart/Smartcontactmanager1Application.java
@@ -7,17 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Smartcontactmanager1Application implements CommandLineRunner  {
+public class Smartcontactmanager1Application implements CommandLineRunner
+{
+	
+  //---------------------------------------------------------------------------------------------
 	
 	@Autowired
 	private BCryptPasswordEncoder bryBCryptPasswordEncoder;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(Smartcontactmanager1Application.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) throws Exception
+	{
 		// TODO Auto-generated method stub
 
 		
